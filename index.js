24118eae67a7082af9c9d2e5cf9322b20122b6a9
Kevin Eslick <eslick.kevin@yahoo.com>
2021-01-30 22:31:35 -0600

    added readme for flavor

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 /*
-Write a javascript program to evaluate a prefix notation string.
-Input operators + * - /. Numbers are all positive integers only. Separated by space character.
-For any such a string the program should return one number which is the revArrult of the calculation.
-The program needs to yield the correct revArrult for valid inputs only, so error handling can be omitted.
-Example:
-"9" => 9
-"+ 1 2" => 3 // This is (1 + 2)
-"+ + 1 2 30" => 33 // This is ((1+2)+30)
-"+ + 12 16 * 10 4" => 68 // This is ((12+16)+(10*4))
+
 */
 
 function prefixEval(expression) {
